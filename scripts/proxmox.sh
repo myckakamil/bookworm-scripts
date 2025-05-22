8332a2f3d9b1e7a1f299c8740cde308eabad7f40
myckakamil <kamil.mycka@protonmail.com>
2025-05-22 14:41:37 +0200

    Creating linux bridge

--- scripts/proxmox.sh
+++ scripts/proxmox.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 DOMENA="test.mycka.net"
 HOSTNAME="proxmox"
 INTERFACE=ens18
@@ -14,6 +16,15 @@ cat << EOF > /etc/hosts
 $IP   $FQDN   $HOSTNAME
 EOF
 
+sed -i s/dhcp/static/g /etc/network/interfaces
+tee -a /etc/network/interfaces << EOF
+auto vmbr0
+iface vmbr0 inet dhcp
+    bridge-ports $INTERFACE
+    bridge-stp off
+    bridge-fd 0
+EOF
+
 echo "deb [arch=amd64] http://download.proxmox.com/debian/pve bookworm pve-no-subscription" > /etc/apt/sources.list.d/pve-install-repo.list
 
 wget https://enterprise.proxmox.com/debian/proxmox-release-bookworm.gpg -O /etc/apt/trusted.gpg.d/proxmox-release-bookworm.gpg 
@@ -21,15 +32,19 @@ wget https://enterprise.proxmox.com/debian/proxmox-release-bookworm.gpg -O /etc/
 sha512sum /etc/apt/trusted.gpg.d/proxmox-release-bookworm.gpg 
 7da6fe34168adc6e479327ba517796d4702fa2f8b4f0a9833f5ea6e6b48f6507a6da403a274fe201595edc86a84463d50383d07f64bdde2e3658108db7d6dc87 /etc/apt/trusted.gpg.d/proxmox-release-bookworm.gpg 
 
-apt update && apt full-upgrade
+apt update && apt full-upgrade -y
 apt install proxmox-default-kernel -y
 apt install proxmox-ve postfix open-iscsi chrony -y
 apt remove linux-image-amd64 'linux-image-6.1*' -y
 update-grub
-apt remove os-prober
+apt remove os-prober -y
 
-echo "Proxmox have been installed. You can access it after reboot"
-echo "It will be available at: ${IP}:8006"
+echo "====================================="
+echo "Proxmox have been installed"
+echo "You can access it after reboot"
+echo "It will be available at: https://${IP}:8006"
+echo "or https://$FQDN:8006 if you set DNS records"
+echo "====================================="
 echo "System will reboot in 5 seconds"
 sleep 5
 systemctl reboot
